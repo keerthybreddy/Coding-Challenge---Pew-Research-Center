e47d596a23728fa49b625cb7a467534e43494785
Keerthy Reddy <102573893+keerthybreddy@users.noreply.github.com>
2025-07-08 12:36:08 -0700

    renaming helloworld.html to index.html to allow for a standard deployed URL

--- tests/hello-test.spec.js
+++ tests/hello-test.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
 test('display Hello World', async ({ page }) => {
-  await page.goto('http://localhost:3000/helloworld.html');
+  await page.goto('http://localhost:3000/');
   await expect(page.locator('h1')).toHaveText('Hello World');
 });
