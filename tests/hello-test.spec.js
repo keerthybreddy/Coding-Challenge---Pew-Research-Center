5d21ae52ba86d79ddf7b92f0900c994dcd6cda37
Keerthy Reddy <102573893+keerthybreddy@users.noreply.github.com>
2025-07-08 12:42:38 -0700

    temporarily breaking playwright test to ensure that app doesn't deploy if test fails

--- tests/hello-test.spec.js
+++ tests/hello-test.spec.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('display Hello World', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await expect(page.locator('h1')).toHaveText('Hello World');
+  await expect(page.locator('h1')).toHaveText('Hello');
 });
