5bd5570eb1768a7bbb9fea8fc9f694df00a9766e
Keerthy Reddy <102573893+keerthybreddy@users.noreply.github.com>
2025-07-10 09:12:46 -0700

    add comments to code

--- tests/hello-test.spec.js
+++ tests/hello-test.spec.js
@@ -1,5 +1,8 @@
+// Playwright test to check if the page is displaying the correct HTML
+
 const { test, expect } = require('@playwright/test');
 
+// This test uses an asynchronous function to navigate to the local environment URL and checks whether the page displays the text "Hello World" using h1 tags
 test('display Hello World', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.locator('h1')).toHaveText('Hello World');
